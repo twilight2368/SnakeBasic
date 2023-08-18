29f426102bd0ec5a639e4bc1b52c526736bfeb5c
Twilight <cuccubird02@gmail.com>
2023-08-18 20:31:01 +0700

    now

--- hello.py
+++ hello.py
@@ -1,1 +1,17 @@
-print("hello");
+import math
+
+print("Hello", end=", ")
+
+age = 15.5
+
+print("I am ", math.ceil(age), "years old")
+
+print(age)
+
+print(10//3)
+
+print(10%3)
+
+print(3**5)
+
+print(math.pow(3,5))
