bf001c614d657891d26694b870ed487dd3483379
Twilight <cuccubird02@gmail.com>
2023-08-19 14:59:43 +0700

    now

--- hello.py
+++ hello.py
@@ -1,24 +1,60 @@
 import math
 
-print("Hello", end=", ")
+# print("Hello", end=", ")
 
-age = 15.5
+# age = 15.5
 
-print("I am ", math.ceil(age), "years old")
+# print("I am ", math.ceil(age), "years old")
 
-print(age)
+# print(age)
 
-print(10//3)
+# print(10//3)
 
-print(10%3)
+# print(10%3)
 
-print(3**5)
+# print(3**5)
 
-print(math.pow(3,5))
+# print(math.pow(3,5))
 
-mess = "Hello"
+# mess = "Hello"
 
-print(mess)
+# print(mess)
 
-print(' yo"lo ')
-print(" yo'lo ")
+# print(' yo"lo ')
+# print(" yo'lo ")
+# print("hello" , 1) 
+
+# age = [1, 2, 3]
+
+# people = ["mike", "gumball", "darwin"]
+
+# my_fav = [ 23, "game", ["the amazing of gumball", "adventure, time"]]
+
+# mess = "hello"
+
+# print(id(mess))
+
+#mess[0] = "H"
+
+# print(mess, id(mess))
+
+# array = [1,2,3]
+# copy = array
+# print(array)
+# copy[0] = -1
+# print(array)
+
+# copy2 = array[:]
+# copy2[0] = 1
+# print (array, copy2)
+
+# my_fav = ["walking", 23, ["gumball", "darwin"]]
+
+# copy = my_fav.copy()
+
+# print(my_fav, copy)
+
+# copy[2][0] = "richard"
+
+# print(my_fav)
+
